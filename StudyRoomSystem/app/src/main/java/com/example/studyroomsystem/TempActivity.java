59d57af9d4130bec02f14091b7b932cdb36b1a32
minjib <48316078+minjib@users.noreply.github.com>
2019-04-28 15:31:09 +0900

    master branch 파일과 병합

--- StudyRoomSystem/app/src/main/java/com/example/studyroomsystem/TempActivity.java
+++ StudyRoomSystem/app/src/main/java/com/example/studyroomsystem/TempActivity.java
@@ -1,7 +1,11 @@
+
 package com.example.studyroomsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TempActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class TempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+        Button btnPersonal_info = (Button)findViewById(R.id.btnPersonal_info);
+        btnPersonal_info.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent in = new Intent(TempActivity.this, Personal_Info.class);
+                startActivity(in);
+            }
+        });
     }
 }

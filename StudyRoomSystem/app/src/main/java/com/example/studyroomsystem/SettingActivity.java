3d7014e51218ff6a0bac08cfffdaa31c6387653e
Snailpong <tjdtnsu@gmail.com>
2019-05-04 11:32:14 +0900

    Logout

--- StudyRoomSystem/app/src/main/java/com/example/studyroomsystem/SettingActivity.java
+++ StudyRoomSystem/app/src/main/java/com/example/studyroomsystem/SettingActivity.java
@@ -1,13 +1,45 @@
 package com.example.studyroomsystem;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SettingActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener{
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_user_reservation:
+                break;
+            case R.id.btn_user_info:
+                break;
+            case R.id.btn_alarm_message:
+                break;
+            case R.id.btn_logout:
+                mAuth.signOut();
+                SharedPreferences pref = getSharedPreferences( "pref" , MODE_PRIVATE);
+                SharedPreferences.Editor ed = pref.edit();
+                ed.remove("id");
+                ed.remove("pw");
+                ed.commit();
+                break;
+            case R.id.btn_getout:
+                break;
+        }
     }
 }

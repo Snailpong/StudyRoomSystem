9a03242f1866a5553efe554570d681d1436b8af4
Snailpong <tjdtnsu@gmail.com>
2019-05-05 16:10:55 +0900

    Buttons2

--- StudyRoomSystem/app/src/main/java/com/example/studyroomsystem/RatioFragment.java
+++ StudyRoomSystem/app/src/main/java/com/example/studyroomsystem/RatioFragment.java
@@ -1,5 +1,6 @@
 package com.example.studyroomsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,58 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 // 서비스2 : 건물별 전체 예약률 제공
 public class RatioFragment extends Fragment {
+    private FirebaseAuth firebaseAuth;
+
+    private TextView textViewUserName;
+    private TextView textViewUserSchoolid;
+    private DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference("users");
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ratio, container, false);
+        View view =  inflater.inflate(R.layout.fragment_ratio, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        String userId = user.getUid();
+        DatabaseReference myRef;
+        DataSnapshot dataSnapshot;
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("users").child(userId);
+        textViewUserName = (TextView) view.findViewById(R.id.textViewUserName);
+        textViewUserSchoolid = (TextView) view.findViewById(R.id.textViewUserSchoolid);
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot item : dataSnapshot.getChildren()) {
+                    if (item.getKey().equals("name"))
+                        name = item.getValue(String.class);
+
+                    if (item.getKey().equals("schoolid"))
+                        schoolid = item.getValue(String.class);
+                }
+                textViewUserName.setText("이름: "+name);
+                textViewUserSchoolid.setText("학번: "+schoolid);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                throw error.toException();
+            }
+        });
+        Button btnModify = (Button) view.findViewById(R.id.btnModify);
+        btnModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(view.getContext(), ModifyPI.class);
+                startActivity(in);
+            }
+        });
+
+        return view;
     }
 }
